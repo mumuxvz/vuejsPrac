ecaa743e9d630e5ace809c61fa38a1a8c337b2ed
Mumux Zadafiya <mumuxzadafiya@gmail.com>
2024-08-23 07:36:54 +0530

    new demo

---
+++ vueDemo/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <firstcomp text="hello from atom"></firstcomp>
+</template>
+
+<script>
+import firstcomp from "../components/firstcomp.vue";
+
+export default {
+  name:'HomeView',
+  components: {
+    firstcomp
+  }
+}
+</script>
